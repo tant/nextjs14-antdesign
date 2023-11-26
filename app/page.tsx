ea35c828b8bffec5bbad2163e3c79b13e708127c
Tan Tran <2188362+tant@users.noreply.github.com>
2023-11-26 20:08:23 +0700

    first commit, still need to fix image tag

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import {
+  Button,
+  Space,
+  Divider,
+} from 'antd';
+
+const Home = function Home() {
+  return (
+    <>
+      <section style={{ textAlign: 'center', marginTop: 48, marginBottom: 40, padding: 100 }}>
+        <Space align='start'>
+          <img
+            style={{ width: 40, height: 40 }}
+            src='https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg'
+            alt='Ant Design'
+          />
+          <h2 style={{ marginBottom: 0 }}>
+            Ant Design (Without Sub Components)
+          </h2>
+        </Space>
+        <Divider style={{ marginBottom: 60 }}>Divider</Divider>
+        <Button type='primary' block href='/sub'>With Sub Components</Button>
+      </section>
+    </>
+  );
+}
+
+export default Home;
